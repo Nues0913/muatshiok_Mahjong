cbe125c840354003893d5e8982c2ea2c36b320bc
Nues0913 <106960429+Nues0913@users.noreply.github.com>
2024-07-01 00:59:34 +0800

    COOL JOKE

--- Assets/Sprite/Square.cs
+++ Assets/Sprite/Square.cs
@@ -2,6 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// 嗎的勒 剛剛騎車去加油
+// 員工問我要加什麼油？
+// 我說 95加100 
+// 結果他回我195 
+// 真的是他媽幽默啊 ！！
+// 天氣很熱就在躁了 還在給我講笑話
+
 public class Square : MonoBehaviour{
     // Start is called before the first frame update
     void Start(){
